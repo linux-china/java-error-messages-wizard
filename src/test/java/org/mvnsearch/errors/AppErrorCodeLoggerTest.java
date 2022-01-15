3622c110cbfe04ef1cc3def01bded620fa38685d
linux_china <libing.chen@gmail.com>
2022-01-14 18:50:02 -0800

    Introduce AppErrorCodeLoggerFactory

--- src/test/java/org/mvnsearch/errors/AppErrorCodeLoggerTest.java
+++ src/test/java/org/mvnsearch/errors/AppErrorCodeLoggerTest.java
@@ -1,17 +1,17 @@
 package org.mvnsearch.errors;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.FormattingTuple;
 import org.slf4j.helpers.MessageFormatter;
 
 public class AppErrorCodeLoggerTest {
-    private static final AppErrorCodeLogger log = new AppErrorCodeLogger(LoggerFactory.getLogger(AppErrorCodeLoggerTest.class));
+    private static final AppErrorCodeLogger log = AppErrorCodeLoggerFactory.getLogger(AppErrorCodeLoggerTest.class);
 
     @Test
     public void testErrorMessage() {
-        System.out.println(AppErrorCodeLogger.errorMessage("APP-100-400","korin_heardi@bye.aqi"));
+        System.out.println(AppErrorCodeLogger.errorMessage("APP-100-400", "korin_heardi@bye.aqi"));
     }
+
     @Test
     public void testPrintErrorMessage() {
         log.info("APP-100-400", "demo@example.com");
